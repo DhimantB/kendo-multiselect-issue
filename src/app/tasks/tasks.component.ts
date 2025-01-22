cebfbceeaa90cef6f70b9ad097500d9fa0e661b8
Dhimant Bhensdadia <patel.erdhimant@gmail.com>
2025-01-22 09:46:43 -0500

    tasks files

---
+++ src/app/tasks/tasks.component.ts
@@ -0,0 +1,55 @@
+import {Component} from '@angular/core';
+import {MultiSelectComponent} from '@progress/kendo-angular-dropdowns';
+import {FormsModule} from '@angular/forms';
+import {TaskService} from './task.service';
+import {ListComponent} from './list/list.component';
+import {PreventableEvent} from '@progress/kendo-angular-common';
+
+@Component({
+    selector: 'app-tasks',
+    imports: [
+        MultiSelectComponent,
+        FormsModule,
+        ListComponent,
+    ],
+    templateUrl: './tasks.component.html',
+    standalone: true,
+    styleUrl: './tasks.component.scss'
+})
+export class TasksComponent {
+
+    filters: any = [
+        {id: 1, description: 'Breakfast'},
+        {id: 2, description: 'Brunch'},
+        {id: 3, description: 'Lunch'},
+        {id: 4, description: 'Dinner'},
+        {id: 5, description: 'Snacks'}
+    ];
+
+    selectedFilters: number[] = [];
+
+    constructor(private _taskService: TaskService) {
+    }
+
+    ngOnInit(): void {
+    }
+
+    refresh() {
+        this._taskService._notifyOnRefreshLogs.next(this.selectedFilters);
+    }
+
+
+    refreshLogs() {
+        this._taskService._notifyOnRefreshLogs.next(this.selectedFilters);
+    }
+
+    onClose($event: any) {
+        console.log('on close');
+        $event.preventDefault();
+    }
+
+    onInputBlur($event: any, multiSelect: MultiSelectComponent) {
+        console.log('onInputBlur');
+        multiSelect.toggle(false);
+    }
+}
