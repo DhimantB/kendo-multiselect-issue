2c8a625871f2153f268d304004870447a708c89f
Dhimant Bhensdadia <patel.erdhimant@gmail.com>
2025-01-22 09:41:35 -0500

    uploaded the src files

---
+++ src/polyfills.ts
@@ -0,0 +1,6 @@
+import '@angular/localize/init'; // Run `npm install --save classlist.js`. // Run `npm install --save web-animations-js`.
+/**
+ * Zone JS is required by default for Angular itself.
+ */
+/*import './zone-flags';*/
+import 'zone.js'; // Included with Angular CLI.
