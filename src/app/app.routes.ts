f05fc77c309cab03c55bc007396c47e5482ba581
Dhimant Bhensdadia <patel.erdhimant@gmail.com>
2025-01-22 10:23:49 -0500

    Add files via upload

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
 import {DashboardComponent} from './dashboard/dashboard.component';
 import {TasksComponent} from './tasks/tasks.component';
+import {TasksGridComponent} from './tasks-grid/tasks-grid.component';
 
 export const routes: Routes = [
-    { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
-    { path: 'dashboard', component: DashboardComponent },
-    { path: 'tasks', component: TasksComponent },
+    {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
+    {path: 'dashboard', component: DashboardComponent},
+    {path: 'tasks', component: TasksComponent},
+    {path: 'tasks-grid', component: TasksGridComponent},
 ];
