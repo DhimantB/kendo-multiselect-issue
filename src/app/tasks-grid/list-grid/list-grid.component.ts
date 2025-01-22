f5e9f234016853020e2ccf530fc0f61caf8d8032
Dhimant Bhensdadia <patel.erdhimant@gmail.com>
2025-01-22 10:25:33 -0500

    Add files via upload

---
+++ src/app/tasks-grid/list-grid/list-grid.component.ts
@@ -0,0 +1,102 @@
+import {Component, OnInit} from '@angular/core';
+import {GridComponent} from '@progress/kendo-angular-grid';
+import {SchedulerEvent} from '@progress/kendo-angular-scheduler';
+import {TaskService} from '../../tasks/task.service';
+
+@Component({
+    selector: 'app-list-grid',
+    imports: [
+        GridComponent
+    ],
+    templateUrl: './list-grid.component.html',
+    standalone: true,
+    styleUrl: './list-grid.component.scss'
+})
+export class ListGridComponent implements OnInit {
+    public events: SchedulerEvent[] = [];
+
+    public orgEvents: SchedulerEvent[] = [
+        {
+            id: 1,
+            title: 'Breakfast',
+            start: new Date('2018-10-22T09:00:00'),
+            end: new Date('2018-10-22T09:30:00'),
+            recurrenceRule: 'FREQ=DAILY;COUNT=1;',
+            dataItem: 1
+        },
+        {
+            id: 2,
+            title: 'Brunch',
+            start: new Date('2018-10-20T09:00:00'),
+            end: new Date('2018-10-20T09:30:00'),
+            recurrenceRule: 'FREQ=DAILY;COUNT=1;',
+            dataItem: 2
+        },
+        {
+            id: 3,
+            title: 'Lunch',
+            start: new Date('2018-10-10T09:00:00'),
+            end: new Date('2018-10-10T09:30:00'),
+            recurrenceRule: 'FREQ=DAILY;COUNT=1;',
+            dataItem: 3
+        },
+        {
+            id: 4,
+            title: 'Dinner',
+            start: new Date('2018-10-08T09:00:00'),
+            end: new Date('2018-10-08T09:30:00'),
+            recurrenceRule: 'FREQ=DAILY;COUNT=1;',
+            dataItem: 4
+        }, {
+            id: 5,
+            title: 'Breakfast',
+            start: new Date('2018-10-05T09:00:00'),
+            end: new Date('2018-10-05T09:30:00'),
+            recurrenceRule: 'FREQ=DAILY;COUNT=1;',
+            dataItem: 1
+        },
+        {
+            id: 6,
+            title: 'Brunch',
+            start: new Date('2018-10-07T09:00:00'),
+            end: new Date('2018-10-07T09:30:00'),
+            recurrenceRule: 'FREQ=DAILY;COUNT=1;',
+            dataItem: 2
+        },
+        {
+            id: 7,
+            title: 'Lunch',
+            start: new Date('2018-10-09T09:00:00'),
+            end: new Date('2018-10-09T09:30:00'),
+            recurrenceRule: 'FREQ=DAILY;COUNT=1;',
+            dataItem: 3
+        },
+        {
+            id: 8,
+            title: 'Dinner',
+            start: new Date('2018-10-11T09:00:00'),
+            end: new Date('2018-10-11T09:30:00'),
+            recurrenceRule: 'FREQ=DAILY;COUNT=1;',
+            dataItem: 4
+        }
+    ];
+
+    constructor(private _activityLogService: TaskService) {
+        this.events = this.orgEvents;
+    }
+    refreshes12345: number[] = [];
+
+    ngOnInit(): void {
+        this._activityLogService._notifyOnRefreshLogs
+            .subscribe((refreshes: number[]) => {
+                this.refreshes12345 = refreshes;
+
+                if (refreshes.length === 0) {
+                    this.events = this.orgEvents;
+                    //this.events = this.orgEvents;
+                } else {
+                    this.events = this.orgEvents.filter(x => refreshes.includes(x.dataItem));
+                }
+            });
+    }
+}
